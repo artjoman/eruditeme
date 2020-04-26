413e888753f81065366d3de6f432a06c3042aed6
Peteris Arajs <Peteris@Peteriss-MBP.lan>
2020-04-26 10:31:32 +0300

    .

---
+++ public/precache-manifest.11a3ee20c27f63cbe943d9a9d62f70fc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "baf737d6afcf58cd15cff20f1447b7b9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b31192ab6372547d3805",
+    "url": "/static/css/main.44af782f.chunk.css"
+  },
+  {
+    "revision": "f4d916365a9b1098c60f",
+    "url": "/static/js/2.92155d7f.chunk.js"
+  },
+  {
+    "revision": "eacdcc46c2547a6a176d37719fbe19d4",
+    "url": "/static/js/2.92155d7f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b31192ab6372547d3805",
+    "url": "/static/js/main.58771bb6.chunk.js"
+  },
+  {
+    "revision": "ff60e6a4df05d748dcd2",
+    "url": "/static/js/runtime-main.eb807d3d.js"
+  }
+]);
